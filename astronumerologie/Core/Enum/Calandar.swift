f505e783dcf3f14dce60dab4d32d049d6ecf4ffb
Stéphane Bressani <stephane@bressani.dev>
2022-11-05 18:55:26 +0100

    core clean

--- astronumerologie/Core/Enum/Calandar.swift
+++
@@ -1,9 +0,0 @@
-//
-// Created by Stéphane on 01.11.22.
-//
-
-import Foundation
-
-enum Calandar: Int32 {
-    case Julian = 0, Gregorian = 1
-}
--- astronumerologie/Core/Enum/LargerDrawLine.swift
+++
@@ -1,9 +0,0 @@
-//
-// Created by Stéphane on 01.11.22.
-//
-
-import Foundation
-
-enum LargerDrawLine {
-    case Large, Small
-}
