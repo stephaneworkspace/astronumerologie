b2793b783ce9d1b7b397dc179528834d88494386
Stéphane Bressani <stephane@bressani.dev>
2022-11-05 20:05:23 +0100

    remove unused code

--- astronumerologie/ContentView.swift
+++ astronumerologie/ContentView.swift
@@ -8,32 +8,6 @@
 import SwiftUI
 import SweBressaniDev
 
-extension String {
-    func toUnsafePointer() -> UnsafePointer<UInt8>? {
-        guard let data = self.data(using: .utf8) else {
-            return nil
-        }
-
-        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: data.count)
-        let stream = OutputStream(toBuffer: buffer, capacity: data.count)
-        stream.open()
-        let value = data.withUnsafeBytes {
-            $0.baseAddress?.assumingMemoryBound(to: UInt8.self)
-        }
-        guard let val = value else {
-            return nil
-        }
-        stream.write(val, maxLength: data.count)
-        stream.close()
-
-        return UnsafePointer<UInt8>(buffer)
-    }
-
-    func toUnsafeMutablePointer() -> UnsafeMutablePointer<Int8>? {
-        return strdup(self)
-    }
-}
-
 struct ContentView: View {
     @Environment(\.colorScheme) var colorScheme: ColorScheme
     @State private var siSelected: Int = 1
