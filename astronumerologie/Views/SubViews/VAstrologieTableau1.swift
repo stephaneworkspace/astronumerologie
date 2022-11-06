f1fa9b440f1a68bdad3c9d1772e0dfc8c642f242
Stéphane Bressani <stephane@bressani.dev>
2022-11-06 18:47:01 +0100

    bogie tableau 1

--- astronumerologie/Views/SubViews/VAstrologieTableau1.swift
+++ astronumerologie/Views/SubViews/VAstrologieTableau1.swift
@@ -9,46 +9,6 @@ import SwiftUI
 import SweBressaniDev
 import SVGView
 
-extension SweCore.Bodies {
-    func bodieSign(swe: SweCore, swTransit: Bool) -> Int {
-        var res = 1
-        if (!swTransit) {
-            // Natal
-            for b in swe.swec.bodiesNatal {
-                if b.bodie == self.rawValue {
-                    for s in 1...12 {
-                        var pos = Double(s - 1) * 30.0
-                        if pos > 360 {
-                            pos = 360 - pos
-                        }
-                        if b.calc_ut.longitude >= pos && b.calc_ut.longitude <= pos + 30 {
-                            res = s
-                            break
-                        }
-                    }
-                }
-            }
-        } else {
-            // Transit
-            for b in swe.swec.bodiesTransit {
-                if b.bodie == self.rawValue {
-                    for s in 1...12 {
-                        var pos = Double(s - 1) * 30.0
-                        if pos > 360 {
-                            pos = 360 - pos
-                        }
-                        if b.calc_ut.longitude >= pos && b.calc_ut.longitude <= pos + 30 {
-                            res = s
-                            break
-                        }
-                    }
-                }
-            }
-        }
-        return res
-    }
-}
-
 struct VAstrologieTableau1: View {
     @Environment(\.colorScheme) var colorScheme: ColorScheme
     @Binding var swe: SweCore
@@ -65,11 +25,11 @@ struct VAstrologieTableau1: View {
                 Text(swe.swec.text_bodie(i: bodie.rawValue))
             }.frame(maxWidth: .infinity, alignment: .leading).offset(x: 30)
             VStack {
-                SVGView(contentsOf: try! swe.swec.asset_sign(i: bodie.bodieSign(swe: swe, swTransit: false)))
+                SVGView(contentsOf: try! swe.swec.asset_sign(i: swe.bodieSign(bodie: bodie, swTransit: false)))
                     .frame(width: 20.0, height: 20.0)
             }.frame(maxWidth: .infinity, alignment: .leading).offset(x: 180, y: 0)
             VStack {
-                SVGView(contentsOf: try! swe.swec.asset_sign(i: bodie.bodieSign(swe: swe, swTransit: true)))
+                SVGView(contentsOf: try! swe.swec.asset_sign(i: swe.bodieSign(bodie: bodie, swTransit: true)))
                     .frame(width: 20.0, height: 20.0)
             }.frame(maxWidth: .infinity, alignment: .leading).offset(x: 280, y: 0)
         }
