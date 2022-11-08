d1fb8492a4dc2a2be174466077d6d3c6b1e6a40b
Stéphane Bressani <stephane@bressani.dev>
2022-11-08 12:21:06 +0100

    add scroll view

--- astronumerologie/Views/VAstrologie.swift
+++ astronumerologie/Views/VAstrologie.swift
@@ -14,58 +14,59 @@ struct VAstrologie: View {
     @Binding var swBodies: [Bool]
 
     var body: some View {
-        ZStack {
-            VAstrologieZodiac(swe: $swe)
-            ForEach(1...12, id: \.self)  { i in
-                VAstrologieAssetSigns(swe: $swe, sign: i)
-            }
-            ForEach(1...4, id: \.self) { i in
-                let angle = SweCore.Angles(rawValue: Int32(i))!
-                VAstrologieAssetAngles(swe: $swe, angle: angle)
-                VAstrologieAngles(swe: $swe, angle: angle)
-            }
-            VAstrologieHouses(swe: $swe)
-            ForEach(1...12, id: \.self)  { i in
-                VAstrologieAssetHouses(swe: $swe, house: i)
-            }
-            //
-            ForEach(Array(zip(swe.bodiesForLoop.indices, swe.bodiesForLoop)), id: \.1) { i, b in
-                if (swBodies[i]) {
-                    VAstrologieBodies(swe: $swe, bodie: b, swTransit: false)
-                    VAstrologieBodies(swe: $swe, bodie: b, swTransit: true)
-                    VAstrologieAssetBodies(swe: $swe, bodie: b, swTransit: false)
-                    VAstrologieAssetBodies(swe: $swe, bodie: b, swTransit: true)
+        ScrollView {
+            ZStack {
+                VAstrologieZodiac(swe: $swe)
+                ForEach(1...12, id: \.self)  { i in
+                    VAstrologieAssetSigns(swe: $swe, sign: i)
+                }
+                ForEach(1...4, id: \.self) { i in
+                    let angle = SweCore.Angles(rawValue: Int32(i))!
+                    VAstrologieAssetAngles(swe: $swe, angle: angle)
+                    VAstrologieAngles(swe: $swe, angle: angle)
+                }
+                VAstrologieHouses(swe: $swe)
+                ForEach(1...12, id: \.self)  { i in
+                    VAstrologieAssetHouses(swe: $swe, house: i)
+                }
+                //
+                ForEach(Array(zip(swe.bodiesForLoop.indices, swe.bodiesForLoop)), id: \.1) { i, b in
+                    if (swBodies[i]) {
+                        VAstrologieBodies(swe: $swe, bodie: b, swTransit: false)
+                        VAstrologieBodies(swe: $swe, bodie: b, swTransit: true)
+                        VAstrologieAssetBodies(swe: $swe, bodie: b, swTransit: false)
+                        VAstrologieAssetBodies(swe: $swe, bodie: b, swTransit: true)
+                    }
                 }
             }
-        }
-        // Tableau 1
-        VStack(spacing: 0) {
-            ForEach(Array(zip(swe.bodiesForLoop.indices, swe.bodiesForLoop)), id: \.1) { i, b in
-                if (swBodies[i]) {
-                    ZStack(alignment: .topLeading) {
-                        VAstrologieTableau1(swe: $swe, bodie: b)
-                        VAstrologieTableau1Signs(swe: $swe, bodie: b, swTransit: false)
-                        VAstrologieTableau1Signs(swe: $swe, bodie: b, swTransit: true)
+            // Tableau 1
+            VStack(spacing: 0) {
+                ForEach(Array(zip(swe.bodiesForLoop.indices, swe.bodiesForLoop)), id: \.1) { i, b in
+                    if (swBodies[i]) {
+                        ZStack(alignment: .topLeading) {
+                            VAstrologieTableau1(swe: $swe, bodie: b)
+                            VAstrologieTableau1Signs(swe: $swe, bodie: b, swTransit: false)
+                            VAstrologieTableau1Signs(swe: $swe, bodie: b, swTransit: true)
+                        }
+                    }
+                }
+            }
+            // Tableau 2
+            VStack(spacing: 0) {
+                ForEach(swe.bodAng(swBodies: swBodies), id: \.id) { b in
+                    switch (b.bodAng) {
+                    case .Bodie(let bodie):
+                        ZStack(alignment: .topLeading) {
+                            VAstrologieTableau2Bodies(swe: $swe, bodie: bodie, i: b.pos)
+                        }
+                    case .Angle(let angle):
+                        ZStack(alignment: .topLeading) {
+                            VAstrologieTableau2Angles(swe: $swe, angle: angle, i: b.pos)
+                        }
                     }
                 }
             }
         }
-        // Tableau 2
-        /*
-        VStack(spacing: 0) {
-            ForEach(swe.bodAng(swBodies: swBodies), id: \.id) { b in
-                switch (b.bodAng) {
-                case .Bodie(let bodie):
-                    ZStack(alignment: .topLeading) {
-                        VAstrologieTableau2Bodies(swe: $swe, bodie: bodie, i: b.pos)
-                    }
-                case .Angle(let angle):
-                    ZStack(alignment: .topLeading) {
-                        VAstrologieTableau2Angles(swe: $swe, angle: angle, i: b.pos)
-                    }
-                }
-            }
-        }*/
     }
 }
 
