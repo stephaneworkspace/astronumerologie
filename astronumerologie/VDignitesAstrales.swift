331c1e1ec874db3d3a35ab695dae551f2235e592
Stéphane Bressani <stephane@bressani.dev>
2022-11-23 11:40:19 +0100

    avancée

--- astronumerologie/VDignitesAstrales.swift
+++ astronumerologie/VDignitesAstrales.swift
@@ -12,14 +12,42 @@ import SweBressaniDev
 struct VDignitesAstrales: View {
     @Binding var bsSwe: SweCore
     var body: some View {
-        HStack {
-            let b = SweCore.Bodies.Soleil
-            Text(bsSwe.swec.text_bodie(i: b.rawValue))
-            GeometryReader { geometry in
+        VStack {
+            HStack {
+                Text("Diginité et débilité astrales")
+            }
+            HStack {
+                let b = SweCore.Bodies.Neptune
                 SVGView(contentsOf: try! bsSwe.swec.asset_bodie(i: b.rawValue))
                         .frame(width: 25, height: 25)
                         .padding()
-            }
+                let s = SweCore.Signs.Pisces
+                Image(systemName: "wand.and.stars")
+                SVGView(contentsOf: try! bsSwe.swec.asset_sign(i: s.rawValue))
+                        .frame(width: 25, height: 25)
+                        .padding()
+                Image(systemName: "wand.and.stars")
+            }.frame(maxWidth: .infinity, alignment: .leading)
+            HStack {
+                let b = SweCore.Bodies.Saturn
+                SVGView(contentsOf: try! bsSwe.swec.asset_bodie(i: b.rawValue))
+                        .frame(width: 25, height: 25)
+                        .padding()
+                Image(systemName: "wand.and.stars")
+                let s = SweCore.Signs.Aquarius
+                SVGView(contentsOf: try! bsSwe.swec.asset_sign(i: s.rawValue))
+                    .frame(width: 25, height: 25)
+                        .padding()
+                let s2 = SweCore.Signs.Capricorn
+                VStack {
+                    SVGView(contentsOf: try! bsSwe.swec.asset_sign(i: s2.rawValue))
+                        .frame(width: 25, height: 25)
+                            .padding()
+                    Text("").frame(width: 25, height: 1).border(Color.black, width: 1)
+                }.padding(0)
+                Image(systemName: "wand.and.stars")
+            }.frame(maxWidth: .infinity, alignment: .leading)
+            Spacer()
         }
     }
 }
